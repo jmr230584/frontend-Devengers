d105d0dac2fc7a5f69dd39e13a9de2f48bec371f
lustaxbp <luisbordinpereira@gmail.com>
2025-08-27 16:46:44 -0300

    adicionando a lista de filmes em todas as paginas de programação

--- src/pages/PEmBreve/PEmBreve.tsx
+++ src/pages/PEmBreve/PEmBreve.tsx
@@ -2,6 +2,7 @@ import { JSX } from "react";
 import Cabecalho from "../../components/Cabecalho/Cabecalho";
 import Paginas from "../../components/Programacao/Paginas";
 import Navbar2 from "../../components/Navbar/Embreve";
+import Listafilmes from "../../components/ListaFilmes";
 
 function PEmbreve(): JSX.Element {
     return (
@@ -9,6 +10,8 @@ function PEmbreve(): JSX.Element {
             <Cabecalho />
             <Paginas />
             <Navbar2/>
+            <Listafilmes/>
+            
         </>
     );
 }
--- src/pages/PPreVenda/PPrevenda.tsx
+++ src/pages/PPreVenda/PPrevenda.tsx
@@ -2,6 +2,7 @@ import { JSX } from "react";
 import Cabecalho from "../../components/Cabecalho/Cabecalho";
 import Paginas from "../../components/Programacao/Paginas";
 import Navbar3 from "../../components/Navbar/Prevenda";
+import Listafilmes from "../../components/ListaFilmes";
 
 function PPreVenda(): JSX.Element {
     return (
@@ -9,6 +10,7 @@ function PPreVenda(): JSX.Element {
             <Cabecalho />
             <Paginas />
             <Navbar3/>
+            <Listafilmes/>
         </>
     );
 }
